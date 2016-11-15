4a870dc053369ea31e564ec03c8db449d5e3b32f
victor <victor@vbonhomme.fr>
2016-11-15 09:11:00 +0800

    add cli --credentials-name

--- README.md
+++ README.md
@@ -8,12 +8,12 @@ npm install
 
 ## Dev
 ```
-DEBUG=*,-babel DEBUG_COLORS=true pm2 start src/index.js --watch --no-autorestart --name marketDataGateway
+DEBUG=*,-babel DEBUG_COLORS=true pm2 start src/index.js --watch --no-autorestart --name marketDataGateway  -- --credentials-name invesmart.win
 pm2 logs marketDataGateway
 ```
 
 ## Prod
 ```
 npm run compile
-DEBUG=*,-babel DEBUG_COLORS=true pm2 start dist/app.js --name marketDataGateway
+DEBUG=*,-babel DEBUG_COLORS=true pm2 start dist/app.js --name marketDataGateway -- --credentials-name invesmart.win
 ```
--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import createDebug from 'debug';
 import fs from 'fs';
 import path from 'path';
 import grpc from 'grpc';
+import program from 'commander';
 import { upperFirst } from 'lodash';
 import marketDataGatewayGrpc from './marketDataGateway.grpc';
 import mongodb from './mongodb';
@@ -11,6 +12,11 @@ import {
   grpcConfig,
 } from './config';
 import marketDatas from './marketDatas';
+
+program
+  .version('1.0.2')
+  .option('-c, --credentials-name [value]', 'the name of the server ssl credentials .crt/.key')
+  .parse(process.argv);
 
 const grpcUrl = `${grpcConfig.ip}:${grpcConfig.port}`;
 const debug = createDebug(`app ${grpcUrl}`);
@@ -34,8 +40,9 @@ async function main() {
 
     const marketDataProto = grpc.load(__dirname.concat('/marketDataGateway.proto'));
 
-    const sslServerCrtPath = path.join(__dirname, '../crt/server.crt');
-    const sslServerKeyPath = path.join(__dirname, '../crt/server.key');
+    const credentialsName = program.credentialsName || 'server';
+    const sslServerCrtPath = path.join(__dirname, `../crt/${credentialsName}.crt`);
+    const sslServerKeyPath = path.join(__dirname, `../crt/${credentialsName}.key`);
     const sslServerCrt = fs.readFileSync(sslServerCrtPath);
     const sslServerKey = fs.readFileSync(sslServerKeyPath);
 
